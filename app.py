42aa609b32635610a752fcb1e94f9d80c87b031d
dafo16ac <dfogarolo87@gmail.com>
2020-07-03 17:14:39 +0200

    init

--- app.py
+++ app.py
@@ -19,6 +19,68 @@ external_stylesheets = ['https://codepen.io/davifoga/pen/jOWYyyG.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 
+from sklearn import linear_model
+import numpy as np
+import scipy.stats as stat
+
+class LogisticRegression_with_p_values:
+    def __init__(self,*args,**kwargs):#,**kwargs):
+        self.model = linear_model.LogisticRegression(*args,**kwargs)#,**args)
+
+    def fit(self,X,y):
+        self.model.fit(X,y)
+        #### Get p-values for the fitted model ####
+        denom = (2.0 * (1.0 + np.cosh(self.model.decision_function(X))))
+        denom = np.tile(denom,(X.shape[1],1)).T
+        F_ij = np.dot((X / denom).T,X) ## Fisher Information Matrix
+        Cramer_Rao = np.linalg.inv(F_ij) ## Inverse Information Matrix
+        sigma_estimates = np.sqrt(np.diagonal(Cramer_Rao))
+        z_scores = self.model.coef_[0] / sigma_estimates # z-score for eaach model coefficient
+        p_values = [stat.norm.sf(abs(x)) * 2 for x in z_scores] ### two tailed test for p-values
+        self.coef_ = self.model.coef_
+        self.intercept_ = self.model.intercept_
+        #self.z_scores = z_scores
+        self.p_values = p_values
+        #self.sigma_estimates = sigma_estimates
+        #self.F_ij = F_ij
+
+# By typing the code below we will ovewrite a part of the class with one that includes p-values
+# Here's the full source code of the ORIGINAL class: https://github.com/scikit-learn/scikit-learn/blob/7b136e9/sklearn/linear_model/base.py#L362
+class LinearRegression(linear_model.LinearRegression):
+    """
+    LinearRegression class after sklearn's, but calculate t-statistics
+    and p-values for model coefficients (betas).
+    Additional attributes available after .fit()
+    are `t` and `p` which are of the shape (y.shape[1], X.shape[1])
+    which is (n_features, n_coefs)
+    This class sets the intercept to 0 by default, since usually we include it
+    in X.
+    """
+
+    # nothing changes in __init__
+    def __init__(self, fit_intercept=True, normalize=False, copy_X=True,
+                 n_jobs=1):
+        self.fit_intercept = fit_intercept
+        self.normalize = normalize
+        self.copy_X = copy_X
+        self.n_jobs = n_jobs
+
+
+    def fit(self, X, y, n_jobs=1):
+        self = super(LinearRegression, self).fit(X, y, n_jobs)
+
+        # Calculate SSE (sum of squared errors)
+        # and SE (standard error)
+        sse = np.sum((self.predict(X) - y) ** 2, axis=0) / float(X.shape[0] - X.shape[1])
+        se = np.array([np.sqrt(np.diagonal(sse * np.linalg.inv(np.dot(X.T, X))))])
+
+        # compute the t-statistic for each feature
+        self.t = self.coef_ / se
+        # find the p-value for each feature
+        self.p = np.squeeze(2 * (1 - stat.t.cdf(np.abs(self.t), y.shape[0] - X.shape[1])))
+        return self
+
+
 
 if ENV == 'dev':
     debug = True # True ??
