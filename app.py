fe1743045712bc7dac58806876fcd80498f2f1f8
dafo16ac <dfogarolo87@gmail.com>
2020-07-03 13:31:20 +0200

    init

--- app.py
+++ app.py
@@ -77,10 +77,19 @@ class LinearRegression(linear_model.LinearRegression):
 
 
 if ENV == 'dev':
-    debug = False # True ??
+    debug = True # True ??
     server = app.server # ??
     # loan_data_inputs_pd_temp = pd.read_csv('D:\Davide\loan_data_inputs_test.csv')
     file_pd = 'pd_model.sav'
+    file_st_1 = 'lgd_model_stage_1.sav'
+    file_st_2 = 'lgd_model_stage_2.sav'
+    file_ead = 'reg_ead.sav'
+    reg_pd = pickle.load(open(file_pd, 'rb'))
+    reg_lgd_st_1 = pickle.load(open(file_st_1, 'rb'))
+    reg_lgd_st_2 = pickle.load(open(file_st_2, 'rb'))
+    reg_ead = pickle.load(open(file_ead, 'rb'))
+
+    """file_pd = 'pd_model.sav'
     file_st_1 = 'lgd_model_stage_1.sav'
     file_st_2 = 'lgd_model_stage_2.sav'
     file_ead = 'reg_ead.sav'
@@ -91,7 +100,7 @@ if ENV == 'dev':
     with open(file_st_2, 'rb') as file:
         reg_lgd_st_2 = pickle.load(file)
     with open(file_ead, 'rb') as file:
-        reg_ead = pickle.load(file)
+        reg_ead = pickle.load(file)"""
 
 
 
